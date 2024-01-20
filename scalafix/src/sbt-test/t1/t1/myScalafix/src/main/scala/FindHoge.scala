8ac5e45b38620dc8ad03d8af23f0a301e4762149
xuwei-k <6b656e6a69@gmail.com>
2024-01-20 18:17:29 +0900

    wip

--- scalafix/src/sbt-test/t1/t1/myScalafix/src/main/scala/FindHoge.scala
+++ scalafix/src/sbt-test/t1/t1/myScalafix/src/main/scala/FindHoge.scala
@@ -1,14 +1,23 @@
 package fix
 
+import metaconfig.Configured
 import scala.meta.Term
 import scalafix.Patch
 import scalafix.lint.Diagnostic
 import scalafix.lint.LintSeverity
+import scalafix.v1.Configuration
+import scalafix.v1.Rule
 import scalafix.v1.SyntacticDocument
 import scalafix.v1.SyntacticRule
 
-class FindHoge extends SyntacticRule("FindHoge") {
+class FindHoge(myConfig: String) extends SyntacticRule("FindHoge") {
+  def this() = this("default value")
+
+  override def withConfiguration(config: Configuration): Configured[Rule] =
+    config.conf.get[String]("FindHoge").map(x => new FindHoge(x))
+
   override def fix(implicit doc: SyntacticDocument): Patch = {
+    assert(myConfig == "aaaaa", myConfig)
     doc.tree.collect { case t @ Term.Name("hoge") =>
       Patch.lint(
         Diagnostic(
@@ -21,4 +30,3 @@ class FindHoge extends SyntacticRule("FindHoge") {
     }.asPatch
   }
 }
-
