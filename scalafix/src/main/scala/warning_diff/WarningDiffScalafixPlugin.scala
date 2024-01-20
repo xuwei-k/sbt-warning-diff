ae0c749077f439711494a4cec3b402f1f4471fa6
xuwei-k <6b656e6a69@gmail.com>
2024-01-20 10:44:26 +0900

    wip [skip ci]

--- scalafix/src/main/scala/warning_diff/WarningDiffScalafixPlugin.scala
+++ scalafix/src/main/scala/warning_diff/WarningDiffScalafixPlugin.scala
@@ -28,7 +28,7 @@ object WarningDiffScalafixPlugin extends AutoPlugin {
       case _: CrossVersion.Binary =>
         s"""${q(m.organization)} %% ${q(m.name)} % ${q(m.revision)}"""
       case _: CrossVersion.Full =>
-        s"""${q(m.organization)} % ${q(m.name)} % ${q(m.revision)} cross CrossVersion.Full"""
+        s"""${q(m.organization)} % ${q(m.name)} % ${q(m.revision)} cross CrossVersion.full"""
       case _ =>
         s"""${q(m.organization)} % ${q(m.name)} % ${q(m.revision)}"""
     }
