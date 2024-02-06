328104848a963a158929fec5312427bba87f45d5
xuwei-k <6b656e6a69@gmail.com>
2024-02-06 13:03:36 +0900

    override toString

--- core/src/main/scala/warning_diff/FixInput.scala
+++ core/src/main/scala/warning_diff/FixInput.scala
@@ -3,12 +3,15 @@ package warning_diff
 import sjsonnew.BasicJsonProtocol.*
 import sjsonnew.JsonFormat
 import warning_diff.FixInput.SubProject
+import warning_diff.JsonClassOps.*
 
 case class FixInput(
   projects: Seq[SubProject],
   base: String,
   output: String
-)
+) {
+  override def toString = this.toJsonString
+}
 
 object FixInput {
   case class SubProject(
@@ -19,7 +22,9 @@ object FixInput {
     scalacOptions: Seq[String],
     scalaVersion: String,
     dialect: Dialect
-  )
+  ) {
+    override def toString = this.toJsonString
+  }
 
   object SubProject {
     implicit val instance: JsonFormat[SubProject] =
--- core/src/main/scala/warning_diff/FixOutput.scala
+++ core/src/main/scala/warning_diff/FixOutput.scala
@@ -2,12 +2,15 @@ package warning_diff
 
 import sjsonnew.BasicJsonProtocol.*
 import sjsonnew.JsonFormat
+import warning_diff.JsonClassOps.*
 
 case class FixOutput(
   projectId: String,
   sbtConfig: String,
   warnings: Seq[Warning]
-)
+) {
+  override def toString = this.toJsonString
+}
 
 object FixOutput {
   implicit val instance: JsonFormat[FixOutput] =
