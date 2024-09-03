6067be3cffa378f4c8979e1de23f2bdc9bbe0ba2
xuwei-k <6b656e6a69@gmail.com>
2024-09-04 08:36:53 +0900

    direct convert sbt to reviewdog format
    [skip ci]

--- core/src/main/scala/warning_diff/rdf/DiagnosticResult.scala
+++ core/src/main/scala/warning_diff/rdf/DiagnosticResult.scala
@@ -3,6 +3,7 @@ package warning_diff.rdf
 import sjsonnew.BasicJsonProtocol.*
 import sjsonnew.JsonFormat
 import warning_diff.JsonClassOps.*
+import xsbti.Severity
 
 case class DiagnosticResult(
   diagnostics: Seq[Diagnostic],
@@ -12,6 +13,55 @@ case class DiagnosticResult(
 }
 
 object DiagnosticResult {
+  def fromSbt(values: Seq[xsbti.Problem]): DiagnosticResult = {
+    def j2s(j: java.util.Optional[Integer]): Option[Int] =
+      if (j.isPresent) Option(j.get.asInstanceOf[Int]) else None
+
+    DiagnosticResult(
+      diagnostics = values.map { w =>
+        Diagnostic(
+          message = w.message,
+          location = Location(
+            path = {
+              val prefix = "${BASE}/"
+              w.position.sourcePath
+                .map { value =>
+                  if (value.startsWith(prefix)) {
+                    value.drop(prefix.length)
+                  } else {
+                    value
+                  }
+                }
+                .orElse("")
+            },
+            range = Range(
+              start = Position(
+                line = j2s(w.position.startLine).orElse(j2s(w.position.line)),
+                column = j2s(w.position.startColumn)
+              ),
+              end = Position(
+                line = j2s(w.position.endLine()).orElse(j2s(w.position.line)),
+                column = j2s(w.position.endColumn)
+              )
+            )
+          ),
+          severity = Some {
+            w.severity() match {
+              case Severity.Info => "INFO"
+              case Severity.Warn => "WARNING"
+              case Severity.Error => "ERROR"
+            }
+          }
+        )
+      },
+      source = Some(
+        Source(
+          name = "scala"
+        )
+      )
+    )
+  }
+
   def fromWarnings(values: Seq[warning_diff.Warning]): DiagnosticResult =
     DiagnosticResult(
       diagnostics = values.map { w =>
